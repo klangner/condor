755a8de00b60de46c41be2bbcb0706d39ac6160c
Krzysztof Langner <klangner@gmail.com>
2013-12-31 17:07:57 +0100

    Can add multiple documents to the index

--- src/Condor/Index.hs
+++ src/Condor/Index.hs
@@ -1,5 +1,6 @@
 module Condor.Index 
     ( DocName
+    , Index
     , Text
     , add
     , empty
@@ -15,6 +16,7 @@ type DocName = String
 type Text = String
 data Index = Index { index :: Map.Map String [String]
                    }
+                   deriving (Show)
 
 
 -- | Create empty index
@@ -25,7 +27,9 @@ empty = Index Map.empty
 -- | Add document to the index
 add :: DocName -> Text -> Index -> Index
 add d c ix = Index $ foldl f (index ix) (tokenize c)
-    where f i t = Map.insert t [d] i
+    where f i t = case Map.lookup t i of 
+                    Just a -> Map.insert t (d:a) i
+                    Nothing -> Map.insert t [d] i
 
 
 -- | search term in the index
--- test-src/Condor/IndexTest.hs
+++ test-src/Condor/IndexTest.hs
@@ -10,15 +10,27 @@ testCases = [("Index", t) | t <- tests]
 tests :: [Test]
 tests = [ TestCase $ prop_empty
         , TestCase $ prop_size ("doc1", "one two three") 3
-        , TestCase $ prop_search ("doc1", "one two three") "two" ["doc1"]
+        , TestCase $ prop_search "two" "doc1" [("doc1", "one two three")]
+        , TestCase $ prop_search "one" "doc1" [ ("doc1", "one two three") 
+                                              , ("doc2", "forty two")
+                                              ]
+        , TestCase $ prop_search "two" "doc2" [ ("doc1", "one two three") 
+                                              , ("doc2", "forty two")
+                                              ]
         ]
+         
+-- | Helper function to populate index         
+indexFromDocs :: [(DocName, Text)] -> Index
+indexFromDocs ds = foldl f empty ds
+    where f i (d, c) = add d c i
+
          
 prop_empty :: Assertion         
 prop_empty = assertEqual "Empty index has 0 size" 0 (size empty)          
          
-prop_search :: (DocName, Text) -> String -> [DocName] -> Assertion         
-prop_search (d, c) s e = assertEqual ("Search " ++ s) e (search idx s)
-    where idx = add d c empty          
+prop_search :: String -> DocName -> [(DocName, Text)] ->  Assertion         
+prop_search s e ds = assertEqual s True $ elem e (search idx s)
+    where idx = indexFromDocs ds
          
 prop_size :: (DocName, Text) -> Int -> Assertion
 prop_size (d, c) n = assertEqual "Index size" n (size $ add d c empty)         
